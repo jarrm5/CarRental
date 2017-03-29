90d7403f48d092084ac02e1e60c4314501e82ba1
jarrm5 <rinellaj2448@gmail.com>
2017-03-29 13:30:18 -0500

    add database dump

--- Project_3/server/connection.php
+++ Project_3/server/connection.php
@@ -1,12 +1,14 @@
 <?php 
-  $db_hostname = 'localhost';
+  $db_hostname = "";
   $db_database = "car_rental";
-  $db_username = "root";
-  $db_password = "onlyAnChOrS5493!";
+  $db_username = "";
+  $db_password = "";
   
 
  $connection = mysqli_connect($db_hostname, $db_username,$db_password,$db_database);
  
  if (!$connection)
     die("Unable to connect to MySQL: " . mysqli_connect_errno());
+
+//Test comment
 ?>
